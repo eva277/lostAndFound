e7b2d3bed630e0d184527753e7d0bf65c79a0523
eva <eva@eva.com>
2016-05-12 18:32:31 +0200

    First Commit

---
+++ client/app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('foundAndLostApp', [
+  'foundAndLostApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
